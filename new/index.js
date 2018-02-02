030ef46e8b380f35fdfec348bbb996e19f043ff8
Vincent Bruijn <vincent-bruijn@g-star.com>
2018-02-02 16:11:45 +0100

    create dictionary in separate file

--- new/index.js
+++ new/index.js
@@ -1,3 +1,9 @@
+const dictionary = require('./dictionary');
+
+function getRandomNumber(upperBound) {
+  return Math.floor(Math.random() * upperBound);
+}
+
 module.exports.leetify = function(stringToLeet, config) {
   let stringAsArray = stringToLeet.split('');
 
@@ -6,39 +12,9 @@ module.exports.leetify = function(stringToLeet, config) {
     if (!dictEntry) {
       return element;
     }
-    const index = (config && config.predictive) ? 0 : Math.floor(Math.random() * dictEntry.length);
+    const index = (config && config.predictive) ? 0 : getRandomNumber(dictEntry.length);
     return dictEntry[index];
   });
 
   return stringAsArray.join('');
-};
-
-
-const dictionary = {
-  a: ['@', '/-\\', '/\\', '4'],
-  b: ['|3', '13', 'I3', '6', ']3', '!3', '(3', '/3', ')3', '8'],
-  c: ['(', '[', '©', '<'],
-  d: ['|)', '])', '[)', 'I>', '|>'],
-  e: ['3'],
-  f: ['|=', '/=', 'ph', 'PH'],
-  g: ['6', '9', '(_-', '&', '(_+', 'C-'],
-  h: ['|-|', ']-[', '[-]', '|~|', '#', ')-('],
-  i: ['1', '|', '!'],
-  j: ['_|', '_/'],
-  k: ['|<', '|{'],
-  l: ['|_', '1', '1_'],
-  m: ['|\/|', '//\\\\//\\\\', '|v|', ']V[', '/\/\\', '/|\\', '|V|', '//.', '.\\\\\\'],
-  n: ['|\|', ']\[', '/\/'],
-  o: ['0', '()'],
-  p: ['|?', '|>', '9', 'q'],
-  q: ['(_),', '()_', 'O,', '0_'],
-  r: ['2', '|~', '|2', '®'],
-  s: ['5', '$', 'Z', 'z'],
-  t: ['+', '7'],
-  u: ['|_|', '(_)'],
-  v: ['\/', '\\\\\\//'],
-  w: ['\\\\\\//\\\\\\//', '`//', '\\/\\/', '\\\\\'', '\V/'],
-  x: ['}{', '><'],
-  y: ['`/'],
-  z: ['2', '7_', '~/_'],
 };
